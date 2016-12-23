d64f77639edcb447352dfa1c80faaff63543ddef
gaoy <gaoyi_x@hotmail.com>
2016-12-23 16:21:15 +0800

    1223 下午

--- src/main/java/cn/org/mingframework/example/ExampleControler.java
+++ src/main/java/cn/org/mingframework/example/ExampleControler.java
@@ -5,12 +5,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@RequestMapping("/example")
 @Controller
 public class ExampleControler {
 	
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value = "/example"/*, method = RequestMethod.GET*/)
 	public String list(Model uiModel){
 		return "example/list";
+	}
+	
+	@RequestMapping(value = "/main")
+	public String main(Model model){
+		return "mainView";
 	}
 }
