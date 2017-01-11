03380f5ca4d6f4175fa891caed50197f2fd685df
gaoy <gaoyi_x@hotmail.com>
2017-01-11 23:00:11 +0800

    0111

--- src/main/java/cn/org/mingframework/service/system/EntityScan.java
+++ src/main/java/cn/org/mingframework/service/system/EntityScan.java
@@ -6,6 +6,10 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.Query;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
@@ -15,21 +19,18 @@ import org.springframework.stereotype.Service;
 
 import com.google.common.collect.Lists;
 
+import cn.org.mingframework.model.entity.system.database.EntityDescription;
+
 @Service
 public class EntityScan {
-	private String packageName = "cn.org.mingframework.model.entity";
+	private String packageName = "cn/org/mingframework/model/entity";
+	
+	@PersistenceUnit(unitName = "emf")
+	EntityManagerFactory emf;
 	
 	@PostConstruct
 	public void scan() throws IOException{
-		ResourcePatternResolver rpr = new PathMatchingResourcePatternResolver();
-		Resource[] resources = rpr.getResources("classpath*:cn/org/mingframework/model/entity/**/*.class");
-		
-		for(Resource resource : resources){
-			String className = resource.getURL().getPath();
-			className = className.split("(classes/)|(!/)")[1];
-			className = className.replace("/", ".").replace(".class", "");
-			System.out.println(resource.getURL().getPath()+"   "+ className);
-		}
+		System.out.println("This will be executed when app is up.");
 		
 		/*
 		ClassPathResource res = new ClassPathResource("cn.org.mingframework.model.entity");
@@ -41,6 +42,36 @@ public class EntityScan {
 	
 	@PreDestroy
 	public void destroy(){
-		//System.out.println("-----------------------------------------");
+		System.out.println("This will be execute when app is down.");	
+	}
+	
+	public void resetEntities(){
+		
+	}
+	
+	private List<EntityDescription> loadEntitiesFromDB(){
+		Query query = emf.createEntityManager().createNamedQuery("select e from EntityDescription e");
+		return query.getResultList();
+	}
+	
+	private List<EntityDescription> loadEntitiesFromEntity() throws IOException{
+		List<EntityDescription> entities = new ArrayList<EntityDescription>();
+		
+		ResourcePatternResolver rpr = new PathMatchingResourcePatternResolver();
+		Resource[] resources = rpr.getResources("classpath*:" + packageName + "/**/*.class");
+		
+		for(Resource resource : resources){
+			EntityDescription entity = new EntityDescription();
+			String className = resource.getURL().getPath();
+			className = className.split("(classes/)|(!/)")[1];
+			className = className.replace("/", ".").replace(".class", "");
+			
+			entity.setClassName(className);
+			entity.setEntityName(resource.getFilename().replace(".class", ""));
+			
+			entities.add(entity);
+		}
+		
+		return entities;
 	}
 }
