48fa1bcdbb3a0761b797427284c685424aaf3fa5
gaoyi79 <gaoyi_x@hotmail.com>
2017-01-11 17:00:55 -0800

    0111

--- src/main/java/cn/org/mingframework/model/entity/system/database/EntityDescription.java
+++ src/main/java/cn/org/mingframework/model/entity/system/database/EntityDescription.java
@@ -19,6 +19,7 @@ public class EntityDescription implements Serializable {
 	private String entityName;
 	private String className;
 	private String entityDescription;
+
 	private List<FieldDescription> fieldDescription = new ArrayList<FieldDescription>();
 
 	private int version;
@@ -76,5 +77,36 @@ public class EntityDescription implements Serializable {
 
 	public void setVersion(int version) {
 		this.version = version;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((className == null) ? 0 : className.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EntityDescription other = (EntityDescription) obj;
+		if (className == null) {
+			if (other.className != null)
+				return false;
+		} else if (className.equals(other.className))
+			return true;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (id.equals(other.id))
+			return true;
+		return true;
 	}
 }
--- src/main/java/cn/org/mingframework/service/system/EntityScan.java
+++ src/main/java/cn/org/mingframework/service/system/EntityScan.java
@@ -6,6 +6,9 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
@@ -15,22 +18,29 @@ import org.springframework.stereotype.Service;
 
 import com.google.common.collect.Lists;
 
+import cn.org.mingframework.model.entity.system.database.EntityDescription;
+
 @Service
 public class EntityScan {
 	private String packageName = "cn.org.mingframework.model.entity";
 	
+	@PersistenceContext(unitName = "emf")
+	private EntityManager em;
+	
 	@PostConstruct
 	public void scan() throws IOException{
-		ResourcePatternResolver rpr = new PathMatchingResourcePatternResolver();
-		Resource[] resources = rpr.getResources("classpath*:cn/org/mingframework/model/entity/**/*.class");
+		System.out.println("This will be loaded when app is start.");
 		
+		//ResourcePatternResolver rpr = new PathMatchingResourcePatternResolver();
+		//Resource[] resources = rpr.getResources("classpath*: "+ packageName + "/**/*.class");
+		/*
 		for(Resource resource : resources){
 			String className = resource.getURL().getPath();
 			className = className.split("(classes/)|(!/)")[1];
 			className = className.replace("/", ".").replace(".class", "");
 			System.out.println(resource.getURL().getPath()+"   "+ className);
 		}
-		
+		*/
 		/*
 		ClassPathResource res = new ClassPathResource("cn.org.mingframework.model.entity");
 		List files = new ArrayList();
@@ -41,6 +51,37 @@ public class EntityScan {
 	
 	@PreDestroy
 	public void destroy(){
-		//System.out.println("-----------------------------------------");
+		System.out.println("This will be loaded when app is shutdown.");
+	}
+	
+	private List<EntityDescription> findAllEntity() throws IOException{
+		Query query = em.createQuery("select e from EntityDescription e");
+		List<EntityDescription> entities = query.getResultList();
+		List<EntityDescription> localEntities = findAllEntityLocal();
+		for(EntityDescription entity : entities){
+			
+		}
+		
+		return entities;
+	}
+	
+	private List<EntityDescription> findAllEntityLocal() throws IOException{
+		List<EntityDescription> entities = new ArrayList<EntityDescription>();
+		
+		ResourcePatternResolver rpr = new PathMatchingResourcePatternResolver();
+		Resource[] resources = rpr.getResources("classpath*: "+ packageName + "/**/*.class");
+
+		for(Resource resource : resources){
+			String className = resource.getURL().getPath();
+			className = className.split("(classes/)|(!/)")[1];
+			className = className.replace("/", ".").replace(".class", "");
+			EntityDescription entity = new EntityDescription();
+			entity.setClassName(className);
+			entity.setEntityName(resource.getFilename().replace(".class", ""));
+			
+			entities.add(entity);
+		}
+		
+		return entities;
 	}
 }
